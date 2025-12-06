7eb474fe775da6e8ee78d1a04be7aea12ae5b5ed
Viktor Lisitsa <lis.na.mostu@gmail.com>
2025-12-06 19:37:42 +0300

    Added normal version

---
+++ Calculator/Calculator/Calculator/views.py
@@ -0,0 +1,17 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from Calculator import app
+
+@app.route('/')
+@app.route('/maket')
+def maket():
+    """Renders the maket page."""
+    return render_template(
+        'maket.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
